de5ce55885f2548d314950df30d635da5c1f555f
Siarhei Mahlysh <maglish@mail.ru>
2018-11-19 01:11:08 +0300

    Initial commit

---
+++ AllureNunit/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("AllureNunit")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("AllureNunit")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("60647066-3b05-49fa-b249-911d2b3a7942")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ AllureNunit/UnitTest1.cs
@@ -0,0 +1,43 @@
+﻿
+namespace AllureNunit
+{
+    using System;
+
+    using Allure.Commons;
+
+    using NUnit.Allure.Attributes;
+    using NUnit.Allure.Core;
+    using NUnit.Framework;
+
+    [AllureNUnit]
+    [TestFixture]
+    public class UnitTest1
+    {
+ 
+        [Test(Description = "Hello allure")]
+        [AllureTag("Regression")]
+        [AllureSeverity(SeverityLevel.critical)]
+        [AllureIssue("ISSUE-1")]
+        [AllureTms("TMS-12")]
+        [AllureOwner("User")]
+        [AllureSuite("PassedSuite")]
+        [AllureSubSuite("NoAssert")]
+        public void TestMethod1()
+        {
+            Console.WriteLine($"Results are saved into {AllureLifecycle.Instance.ResultsDirectory}");
+         
+            Console.WriteLine("hello allure");
+            Assert.IsTrue(true);
+        }
+
+        [Test]
+        [AllureTag("NUnit", "Debug")]
+        [AllureIssue("GitHub#1", "https://github.com/unickq/allure-nunit")]
+        [AllureFeature("Core")]
+        public void EvenTest([Range(0, 5)] int value)
+        {
+            Assert.IsTrue(value % 2 == 0, $"Oh no :( {value} % 2 = {value % 2}");
+        }
+
+    }
+}
