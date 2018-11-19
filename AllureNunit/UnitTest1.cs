0b88a591e4da4645d1d06d5b7f50a2e4b05be0aa
Siarhei Mahlysh <maglish@mail.ru>
2018-11-19 15:48:11 +0300

    Add More tests

--- AllureNunit/UnitTest1.cs
+++ AllureNunit/UnitTest1.cs
@@ -14,14 +14,16 @@ namespace AllureNunit
     public class UnitTest1
     {
  
-        [Test(Description = "Hello allure")]
+        [Test]
+        [AllureFeature("Core")]
         [AllureTag("Regression")]
         [AllureSeverity(SeverityLevel.critical)]
         [AllureIssue("ISSUE-1")]
         [AllureTms("TMS-12")]
         [AllureOwner("User")]
-        [AllureSuite("PassedSuite")]
-        [AllureSubSuite("NoAssert")]
+        [AllureSuite("Passed tests")]
+        [AllureSubSuite("Search module")]
+        [AllureOwner("Siarhei Mahlysh")]
         public void TestMethod1()
         {
             Console.WriteLine($"Results are saved into {AllureLifecycle.Instance.ResultsDirectory}");
@@ -31,9 +33,13 @@ namespace AllureNunit
         }
 
         [Test]
+        [AllureFeature("Core")]
         [AllureTag("NUnit", "Debug")]
         [AllureIssue("GitHub#1", "https://github.com/unickq/allure-nunit")]
         [AllureFeature("Core")]
+        [AllureSeverity(SeverityLevel.minor)]
+        [AllureOwner("Siarhei Mahlysh")]
+        [AllureSuite("PartiallyFailedSuite")]
         public void EvenTest([Range(0, 5)] int value)
         {
             Assert.IsTrue(value % 2 == 0, $"Oh no :( {value} % 2 = {value % 2}");
---
+++ AllureNunit/UnitTest2.cs
@@ -0,0 +1,66 @@
+﻿namespace AllureNunit
+{
+    using Allure.Commons;
+    using NUnit.Allure.Attributes;
+    using NUnit.Allure.Core;
+    using NUnit.Framework;
+    using System;
+
+    [AllureNUnit]
+    [TestFixture]
+    public class UnitTest2
+    {
+
+        [Test]
+        [Category("Blabla")]
+        [AllureTag("Regression")]
+        [AllureSeverity(SeverityLevel.critical)]
+        [AllureIssue("ISSUE-1")]
+        [AllureTms("TMS-12")]
+        [AllureOwner("User")]
+        [AllureSuite("PassedSuite2")]
+        [AllureSubSuite("Search module")]
+        [AllureOwner("Siarhei Mahlysh")]
+        [AllureSuite("Passed tests")]
+        [AllureFeature("Document")]
+        public void TestMethod2()
+        {
+            Console.WriteLine($"Results are saved into {AllureLifecycle.Instance.ResultsDirectory}");
+
+            Console.WriteLine("hello allure");
+            Assert.IsTrue(true);
+        }
+
+        [Test]
+        [AllureTag("Regression")]
+        [AllureSeverity(SeverityLevel.critical)]
+        [AllureIssue("ISSUE-1")]
+        [AllureTms("TMS-12")]
+        [AllureOwner("User")]
+        [AllureSuite("Skipped tests")]
+        [AllureSubSuite("Search module")]
+        [AllureOwner("Siarhei Mahlysh")]
+        [AllureFeature("Document")]
+        public void SkippedTest()
+        {
+            Console.WriteLine($"Results are saved into {AllureLifecycle.Instance.ResultsDirectory}");
+            Assert.Ignore();
+        }
+
+        [Test]
+        [AllureTag("Regression")]
+        [AllureSeverity(SeverityLevel.critical)]
+        [AllureIssue("ISSUE-1")]
+        [AllureTms("TMS-12")]
+        [AllureOwner("User")]
+        [AllureSuite("Incoclusive tests")]
+        [AllureSubSuite("Search module")]
+        [AllureOwner("Siarhei Mahlysh")]
+        [AllureFeature("Document")]
+        public void IncoclusiveTest()
+        {
+            Console.WriteLine($"Results are saved into {AllureLifecycle.Instance.ResultsDirectory}");
+            Assert.Inconclusive();
+        }
+    }  
+}
